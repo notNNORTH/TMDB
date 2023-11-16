2cbf208998d2f6f903ab10924cb698561d3478b2
星穹铁道高手 <1277526115@qq.com>
2023-11-16 20:31:44 +0800

    小修createDeputyClassImpl逻辑，定位issue2出现在select

--- src/main/java/edu/whu/tmdb/query/operations/impl/CreateDeputyClassImpl.java
+++ src/main/java/edu/whu/tmdb/query/operations/impl/CreateDeputyClassImpl.java
@@ -31,9 +31,7 @@ import edu.whu.tmdb.query.operations.utils.SelectResult;
 public class CreateDeputyClassImpl implements CreateDeputyClass {
     private final MemConnect memConnect;
 
-    public CreateDeputyClassImpl() {
-        this.memConnect = MemConnect.getInstance(MemManager.getInstance());
-    }
+    public CreateDeputyClassImpl() { this.memConnect = MemConnect.getInstance(MemManager.getInstance()); }
 
     @Override
     public boolean createDeputyClass(Statement stmt) throws TMDBException, IOException {
@@ -41,84 +39,73 @@ public class CreateDeputyClassImpl implements CreateDeputyClass {
     }
 
     public boolean execute(net.sf.jsqlparser.statement.create.deputyclass.CreateDeputyClass stmt) throws TMDBException, IOException {
-        // 获取新创建代理类的名称
-        String deputyClassName = stmt.getDeputyClass().toString();
-        int deputyType = getDeputyType(stmt);
-        Select select = stmt.getSelect();
-        // 获取select语句的selectResult
-        SelectResult selectResult = getSelectResult(select);
+        // 1.获取代理类名、代理类型、select元组
+        String deputyClassName = stmt.getDeputyClass().toString();  // 代理类名
         if (memConnect.classExist(deputyClassName)) {
             throw new TMDBException(ErrorList.TABLE_ALREADY_EXISTS, deputyClassName);
         }
+        int deputyType = getDeputyType(stmt);   // 代理类型
+        Select selectStmt = stmt.getSelect();
+        SelectResult selectResult = getSelectResult(selectStmt);
+
+        // 2.执行代理类创建
         return createDeputyClassStreamLine(selectResult, deputyType, deputyClassName);
     }
 
 
-    public boolean createDeputyClassStreamLine(SelectResult selectResult, int deputyType, String deputyClass) throws TMDBException, IOException {
-        int deputyId = createDeputyClass(deputyClass, selectResult, deputyType);
+    public boolean createDeputyClassStreamLine(SelectResult selectResult, int deputyType, String deputyClassName) throws TMDBException, IOException {
+        int deputyId = createDeputyClass(deputyClassName, selectResult, deputyType);
         insertDeputyTable(selectResult.getClassName(), deputyType, deputyId);
         insertTuple(selectResult, deputyId);
         return true;
     }
 
-
-    private List<String> getColumns(Select select) {
-        SelectImpl select1 = new SelectImpl();
-        ArrayList<SelectItem> selectItemList = (ArrayList<SelectItem>) ((PlainSelect) select.getSelectBody()).getSelectItems();
-        HashMap<SelectItem, ArrayList<Column>> selectItemToColumn = select1.getSelectItemColumn(selectItemList);
-        List<Column> selectColumnList = select1.getSelectColumnList(selectItemToColumn);
-        List<String> res = new ArrayList<>();
-        for (int i = 0; i < selectColumnList.size(); i++) {
-            res.add(selectColumnList.get(i).getColumnName());
-        }
-        return res;
-    }
-
+    /**
+     * 给定创建代理类语句，返回代理规则
+     * @param stmt 创建代理类语句
+     * @return 代理规则
+     */
     private int getDeputyType(net.sf.jsqlparser.statement.create.deputyclass.CreateDeputyClass stmt) {
         switch (stmt.getType().toLowerCase(Locale.ROOT)) {
-            case "selectdeputy":
-                return 0;
-            case "joindeputy":
-                return 1;
-            case "uniondeputy":
-                return 2;
-            case "groupbydeputy":
-                return 3;
+            case "selectdeputy":    return 0;
+            case "joindeputy":      return 1;
+            case "uniondeputy":     return 2;
+            case "groupbydeputy":   return 3;
         }
         return -1;
     }
 
-    //将创建deputyclass后面的select语句中的selectResult拿到，用于后面的处理
-    private SelectResult getSelectResult(Select select) throws TMDBException, IOException {
-        SelectImpl select1 = new SelectImpl();
-        SelectResult selectResult = select1.select(select);
-        return selectResult;
+    /**
+     * 给定查询语句，返回select查询执行结果（创建deputyclass后面的select语句中的selectResult）
+     * @param selectStmt select查询语句
+     * @return 查询执行结果（包含所有满足条件元组）
+     */
+    private SelectResult getSelectResult(Select selectStmt) throws TMDBException, IOException {
+        SelectImpl selectExecutor = new SelectImpl();
+        return selectExecutor.select(selectStmt);
     }
 
     //第一步，创建代理类，代理类的classtype设置为de
     //同时，在switchingtable中插入源属性到代理属性的映射
     private int createDeputyClass(String deputyClassName, SelectResult selectResult, int deputyRule) throws TMDBException {
         MemConnect.getClasst().maxid++;
-        int classid = MemConnect.getClasst().maxid;//代理类的id
-        int count = selectResult.getAttrid().length;//代理类的长度
+        int classId = MemConnect.getClasst().maxid;         // 代理类的id
+        int attrNum = selectResult.getAttrid().length;      // 代理类的长度
         for (int i = 0; i < selectResult.getAttrid().length; i++) {
+            // 1.新建classTableItem
             MemConnect.getClasst().classTableList.add(
-                    new ClassTableItem(deputyClassName,
-                            classid,
-                            count,
-                            selectResult.getAttrid()[i],
-                            selectResult.getAttrname()[i],
-                            selectResult.getType()[i],
-                            "de",
-                            ""));
+                    new ClassTableItem(deputyClassName, classId, attrNum, selectResult.getAttrid()[i],
+                            selectResult.getAttrname()[i], selectResult.getType()[i], "de", ""));
+            // 2.新建switchingTableItem
             String className = selectResult.getClassName()[i];
             int oriId = memConnect.getClassId(className);
             int oriAttrId = getOriAttrId(oriId, selectResult.getAlias()[i]);
             MemConnect.getSwitchingT().switchingTableList.add(
-                    new SwitchingTableItem(oriId, oriAttrId, selectResult.getAlias()[i], classid, i, selectResult.getAttrname()[i], deputyRule + "")
+                    new SwitchingTableItem(oriId, oriAttrId, selectResult.getAlias()[i], classId,
+                            i, selectResult.getAttrname()[i], deputyRule + "")
             );
         }
-        return classid;
+        return classId;
     }
 
     /**
@@ -185,4 +172,6 @@ public class CreateDeputyClassImpl implements CreateDeputyClass {
         }
         return res;
     }
+
+
 }
